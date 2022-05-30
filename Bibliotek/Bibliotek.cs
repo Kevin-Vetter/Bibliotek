d5fc451746cd465afc146ec96593689321b00ac3
Kevin Mathias Vetter <kevi2748@elevcampus.dk>
2022-05-30 08:28:05 +0200

    attempt at version 1

---
+++ Bibliotek/Bibliotek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteket
+{
+    internal class Bibliotek
+    {
+        string _biblioteksNavn = string.Empty;
+
+
+        internal Bibliotek(string biblioteksNavn)
+        {
+            _biblioteksNavn = biblioteksNavn;
+        }
+
+        internal string HentBibliotek()
+        {
+            return string.Format($"Velkommen til {_biblioteksNavn} - datoen i dag er: {DateTime.Now.ToShortDateString()}");
+        }
+
+        
+    }
+}
--- Bibliotek/Program.cs
+++ Bibliotek/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Biblioteket;
+Bibliotek bibliotek = new(Console.ReadLine());
+
+Console.WriteLine(bibliotek.HentBibliotek());
